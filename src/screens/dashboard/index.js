2707fe457e0576ac421bc669e1a97a5be2fb0b07
fizan2904 <fizan.nagarchi6@gmail.com>
2022-11-14 01:43:50 +0530

    renamed to dashboard

---
+++ src/screens/dashboard/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+    Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, PointElement,
+} from 'chart.js';
+
+import BubbleChart from './charts/bubble';
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, PointElement);
+
+export default function Dashboard() {
+    const [data, setData] = React.useState([]);
+
+    React.useEffect(() => {
+        const loadData = async () => {
+            const resp = await fetch('http://localhost:3000/calculations/bubbleChart', {
+                credentials: 'include',
+            });
+            const jsonData = await resp.json();
+            setData(jsonData.value);
+        };
+
+        loadData();
+    }, []);
+
+    return (
+        <div>
+            <BubbleChart data={
+                {
+                    labels: ['Red', 'Orange', 'Blue'],
+                    datasets: [
+                        {
+                            data: data.map((doc) => ({
+                                x: doc.diningRating,
+                                y: doc.deliveryRating,
+                                r: doc.price,
+                            })),
+                            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                        },
+                    ],
+                }
+            }
+            />
+        </div>
+    );
+}
